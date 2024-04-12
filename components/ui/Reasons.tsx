7ff7c269992b37ea83111ab2b9554240c4a427a5
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2024-04-12 15:10:30 -0300

    feat: add reasons section

--- components/ui/Reasons.tsx
+++ components/ui/Reasons.tsx
@@ -1,5 +1,68 @@
-export default function Reasons() {
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+
+interface Props {
+    title?: string;
+    subTitle?: string;
+    itemList: {
+        image?: ImageWidget;
+        imageWidthMobile?: string;
+        imageHeightMobile?: string;
+        imageWidthDesktop?: string;
+        imageHeightDesktop?: string;
+        title?: string;
+        text?: string;
+    }[];
+}
+
+export default function Reasons({ title, subTitle, itemList }: Props) {
     return (
-        <div>VEM REASON</div>
-    )
+        <div class="py-[90px] bg-white">
+            <div class="px-4 lg:px-10 xl:w-[1200px] xl:mx-auto xl:px-0">
+                <div class="px-4 lg:px-10 xl:w-[1200px] xl:mx-auto xl:px-0">
+                    <h2 class="text-center text-[#167BF7] text-[27px] font-black lg:text-[40px]">
+                        {title}
+                    </h2>
+                    <p class="text-[16px] mt-4 mb-8 flex justify-center text-[#093366]">
+                        {subTitle}
+                    </p>
+                    <ul class="list-none flex flex-wrap justify-between">
+                        {itemList.map((item) => (
+                            <li class="mb-[55px] mr-[30px] md:w-[40%] lg:w-[18%] lg:mr-0">
+                                {item.image && (
+                                    <>
+                                        <Image
+                                            class="md:hidden"
+                                            src={item.image}
+                                            alt="Ícone da lista"
+                                            width={item.imageWidthMobile}
+                                            height={item.imageHeightMobile}
+                                        />
+                                        <Image
+                                            class="hidden md:block"
+                                            src={item.image}
+                                            alt="Ícone da lista"
+                                            width={item.imageWidthDesktop}
+                                            height={item.imageHeightDesktop}
+                                        />
+                                    </>
+                                )}
+                                <h3 class="text-[#093366] text-[22px] my-[13px] font-bold">
+                                    {item.title}
+                                </h3>
+                                {item.text && (
+                                    <p
+                                        class="text-[16px] mt-[7px] leading-[1.4] text-[#093366]"
+                                        dangerouslySetInnerHTML={{
+                                            __html: item.text,
+                                        }}
+                                    ></p>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
--- components/ui/Registration.tsx
+++ components/ui/Registration.tsx
@@ -8,28 +8,30 @@ interface Props {
 export default function Registration(
   { principalText, subText, buttonText, buttonUrl }: Props,
 ) {
-  <div
-    class="pb-12"
-    style="background-image:linear-gradient(to bottom,transparent -35%,#093366 50%);"
-  >
-    <div class="px-4">
-      <div
-        style={`background-image:url(/image/matricula-bg.svg); background-repeat:no-repeat; background-size:110% 120%; background-position:center;`}
-        class="px-10 pt-10 pb-12 rounded-[5px] text-white text-center flex flex-col items-center"
-      >
-        <div class="mb-8">
-          <p class="text-white text-[22px] font-bold">{principalText}</p>
-          <p class="text-white text-[22px]">{subText}</p>
-        </div>
-        <a
-          href={buttonUrl}
-          class="rounded-[32.5px] bg-white text-[#167bf7] py-5 px-[60px] uppercase font-bold cursor-pointer w-[17em]"
+  return (
+    <div
+      class="pb-12 bg-[#051933]"
+      style="background-image:linear-gradient(to bottom,transparent -35%,#093366 50%);"
+    >
+      <div class="px-4 ml2:px-[30px] xl:px-0 xl:w-[1200px] xl:max-w-[1280px] xl:mx-auto">
+        <div
+          style={`background-image:url(/image/matricula-bg.svg); background-repeat:no-repeat; background-size:110% 120%; background-position:center;`}
+          class="px-10 pt-10 pb-12 rounded-[5px] text-white text-center flex flex-col items-center"
         >
-          {buttonText}
-        </a>
+          <div class="mb-8">
+            <p class="text-white text-[22px] font-bold">{principalText}</p>
+            <p class="text-white text-[22px]">{subText}</p>
+          </div>
+          <a
+            href={buttonUrl}
+            class="rounded-[32.5px] bg-white text-[#167bf7] py-5 px-[60px] uppercase font-bold cursor-pointer w-[17em]"
+          >
+            {buttonText}
+          </a>
+        </div>
+      </div>
+      <div>
       </div>
     </div>
-    <div>
-    </div>
-  </div>;
+  );
 }
